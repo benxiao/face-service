e742faed55a09aab3fb2d9bb12b628d2c8963e2f
ranxiao <benji.xiao@gmail.com>
2019-11-26 13:29:43 +1100

    make the client code dry

--- client.py
+++ client.py
@@ -10,38 +10,43 @@ def cli():
     pass
 
 
-@click.command()
-@click.argument('name')
-@click.argument('filename')
-def remember(name, filename):
-    if not os.path.isfile(filename):
-        raise ValueError(f"{filename} not found")
-    photo_bytes = open(filename, "rb").read()
-    base64encoded = base64.b64encode(photo_bytes)
-    json_obj = { "img" : base64encoded.decode("utf-8") }
-    json_str = json.dumps(json_obj)
-    resp = requests.post(f"http://0.0.0.0:5001/remember/{name}", json=json_str)
-    print(resp.status_code)
-    print(resp.content)
-
-
-@click.command()
-@click.argument('filename')
-def recognize(filename):
+def face_command(cmd, **kwargs):
+    filename = kwargs['filename']
     if not os.path.isfile(filename):
         raise ValueError(f"{filename} not found")
     photo_bytes = open(filename, "rb").read()
     base64encoded = base64.b64encode(photo_bytes)
     json_obj = {"img": base64encoded.decode("utf-8")}
     json_str = json.dumps(json_obj)
-    resp = requests.post(f"http://0.0.0.0:5001/recognize", json=json_str)
-    print(resp.status_code)
+
+    if cmd is "remember":
+        resp = requests.post(f"http://0.0.0.0:5001/remember/{kwargs['name']}", json=json_str)
+    elif cmd is "recognize":
+        resp = requests.post(f"http://0.0.0.0:5001/recognize", json=json_str)
+    else:
+        raise(ValueError("unknown command!"))
+    return resp
+
+
+@click.command()
+@click.argument('name')
+@click.argument('filename')
+def remember(name, filename):
+    resp = face_command("remember", name=name, filename=filename)
+    print("status_code:", resp.status_code)
+    print(resp.content)
+
+
+@click.command()
+@click.argument('filename')
+def recognize(filename):
+    resp = face_command("recognize", filename=filename)
+    print("status_code:", resp.status_code)
     print(resp.content)
 
 
 cli.add_command(remember)
 cli.add_command(recognize)
-
 
 if __name__ == '__main__':
     cli()

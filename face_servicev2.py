244c32d0dbb7e73300375497811d9f91da9ffcea
ranxiao <benji.xiao@gmail.com>
2019-11-28 15:05:26 +1100

    reimplement face service using flask-restful

---
+++ face_servicev2.py
@@ -0,0 +1,155 @@
+import json
+import base64
+from io import BytesIO
+import imghdr
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+import numpy as np
+import face_recognition
+from waitress import serve
+from pyfiglet import Figlet
+
+from db import FaceDB
+
+
+tolerance = 0.6
+app = Flask(__name__)
+api = Api(app)
+db = FaceDB()
+
+RC_FORBIDDEN = 403
+RC_BAD_REQUEST = 400
+RC_NOT_FOUND = 404
+RC_CONFLICT = 409
+RC_OK = 200
+
+
+def get_encodings_from_photo(photo_in_bytes):
+    photo_array = face_recognition.load_image_file(BytesIO(photo_in_bytes))
+    locations = face_recognition.face_locations(photo_array)
+    return face_recognition.face_encodings(photo_array, locations)
+
+
+def recognize_from(face_encoding_lookup, face_encoding):
+    return min((
+        (name, np.linalg.norm(e - face_encoding))
+        for name, e
+        in face_encoding_lookup.items()
+    ), key=lambda x: x[1])
+
+
+class RememberFace(Resource):
+    def post(self, username):
+        face_encodings = db.load_table()
+        if username in face_encodings:
+            return {"error": "name already exists"}, RC_FORBIDDEN
+
+        json_str = request.get_json()
+
+        if json_str is None:
+            return {"error": "no json payload!"}, RC_BAD_REQUEST
+
+        try:
+            json_object = json.loads(json_str)
+        except:
+            return {"error": "not valid json"}, RC_BAD_REQUEST
+
+        if json_object.get('img') is None:
+            return {"error": "bad payload!"}, RC_BAD_REQUEST
+
+        image_bytes = base64.b64decode(json_object['img'].encode('utf-8'))
+        inferred_format = imghdr.what(BytesIO(image_bytes))
+        if inferred_format not in ("jpeg", "jpg"):
+            return {"error": f"img is not a valid jpg, its a {inferred_format}"}, RC_BAD_REQUEST
+
+        encodings = get_encodings_from_photo(image_bytes)
+        if len(encodings) == 0:
+            return {"error": "no face in photo"}, RC_BAD_REQUEST
+
+        if len(encodings) > 1:
+            return {"error": "more then one face in photo"}, RC_BAD_REQUEST
+
+        encoding = encodings[0]
+        dist = tolerance
+        if face_encodings:
+            _, dist = recognize_from(face_encodings, encoding)
+
+        if dist < tolerance:
+            return {"error": "face already exist!"}, RC_CONFLICT
+
+        db.remember(username, encoding)
+        return {"message": f"{username} added"}, RC_OK
+
+
+class ListUsers(Resource):
+    def get(self):
+        return list(db.load_table()), RC_OK
+
+
+class FaceEncoding(Resource):
+    def get(self, username):
+        face_encodings = db.load_table()
+        if username not in face_encodings:
+            return {"error": "name doesn't exist"}, RC_NOT_FOUND
+        return {"name": username, "encoding": list(face_encodings[username])}, RC_OK
+
+
+class FaceRecognition(Resource):
+    def post(self):
+        json_str = request.get_json()
+        if json_str is None:
+            return {"error": "no json payload"}
+
+        try:
+            json_object = json.loads(json_str)
+        except:
+            return {"error": "not valid json"}, RC_BAD_REQUEST
+
+        if json_object.get('img') is None:
+            return {"error": "bad payload"}
+
+        image_bytes = base64.b64decode(json_object['img'].encode('utf-8'))
+        inferred_format = imghdr.what(BytesIO(image_bytes))
+        if inferred_format not in ("jpeg", "jpg"):
+            return {"error": f"img is not a valid jpg, its a {inferred_format}"}
+
+        encodings = get_encodings_from_photo(image_bytes)
+        if len(encodings) == 0:
+            return {"error": "no face in photo"}
+
+        if len(encodings) > 1:
+            return {"error": "more than one face in photo"}
+
+        encoding = encodings[0]
+
+        name, dist = recognize_from(db.load_table(), encoding)
+        if dist > tolerance:
+            return {"error": "new face!"}, RC_NOT_FOUND
+
+        return {
+            "person": name,
+            "dist": dist
+        }, RC_OK
+
+
+class ForgetFace(Resource):
+    def delete(self, username):
+        face_encodings = db.load_table()
+        if username not in face_encodings:
+            return {"error": "name does not exist"}, RC_NOT_FOUND
+
+        db.forget(username)
+        return {"message": f"{username} is deleted"}, RC_OK
+
+
+api.add_resource(RememberFace, '/face/remember/<string:username>')
+api.add_resource(ListUsers, '/face/list-names')
+api.add_resource(FaceEncoding, '/face/encoding/<string:username>')
+api.add_resource(FaceRecognition, '/face/recognize')
+api.add_resource(ForgetFace, '/face/forget/<string:username>')
+
+
+if __name__ == '__main__':
+    print(Figlet().renderText("Restful-Faces"))
+    serve(app, port=5001)
